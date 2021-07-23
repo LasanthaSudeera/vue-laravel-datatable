ff9ad99d8de0a48eeff2ffc91fabc0af6fa9b653
LasanthaSudeera <lasan.sudeera@gmail.com>
2021-07-23 14:06:56 +0530

    Initial commit

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import VueLaravelDatatable from '@/vue-laravel-datatable.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    VueLaravelDatatable
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-laravel-datatable />
+  </div>
+</template>
